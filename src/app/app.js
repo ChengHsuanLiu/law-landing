f4b13f9d1eb60ab63e793c60f4a3d6ebcee02e82
YuTin Liu <yuting1987@gmail.com>
2015-12-30 19:49:08 +0800

    init

---
+++ src/app/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import c from 'classnames';
+
+import style from './app.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className={style.nav}>
+          <div>呼叫律師</div>
+          <div>
+            <a>如何使用</a>
+            <a>費用說明</a>
+          </div>
+        </div>
+        <div className={style.main}>
+          <img src="./main-text.png" />
+          <a className={style.enterBtn}>
+            開始使用
+          </a>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
